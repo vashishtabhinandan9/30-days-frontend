8f17239388546c599c7ba99c0fc3703a1e1d448e
vashishtabhinandan9 <vashishtabhinandan9@gmail.com>
2022-05-10 22:00:14 +0530

    day 3 htmlandcss done

---
+++ day3/index.js
@@ -0,0 +1,22 @@
+
+
+  call=()=>{ consol.log("hello")
+}
+
+
+    const arr=[
+      [1,2],
+      "a",
+    call
+    ]
+
+  
+
+  arr.forEach((x)=>{
+    console.log(x)
+  })
+
+  
+    arr[2]()
+
+  
